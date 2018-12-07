975fac163c591aff64b35cee827cb2cfe03a9d9c
Владислав <vladislav.gorde@yandex.ru>
2018-12-07 20:22:24 +0300

    Это тринадцатый коммит

---
+++ js/javascript/script.js
@@ -0,0 +1,6 @@
+	// var frm = document.getElementById('formBlock');
+	// function valid(form) {
+	// 	var name = frm.name.value;
+	// 	alert(name);
+	// }
+	// frm.addEventListener(onsubmit, valid("frm")); 
